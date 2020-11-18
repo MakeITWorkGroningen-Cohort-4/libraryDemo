70672988021e81bc902cb0f323353c3f3e9c3ccb
Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
2020-11-18 11:06:10 +0100

    added a book details page

--- src/main/java/nl/miwgroningen/cohort4/vincent/libraryDemo/controller/BookController.java
+++ src/main/java/nl/miwgroningen/cohort4/vincent/libraryDemo/controller/BookController.java
@@ -9,7 +9,10 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Optional;
 
 /**
  * @author Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
@@ -30,6 +33,16 @@ public class BookController {
     protected String showBooks(Model model) {
         model.addAttribute("allBooks", bookRepository.findAll());
         return "bookOverview";
+    }
+
+    @GetMapping("/book/{bookTitle}")
+    protected String showBookDetails(Model model, @PathVariable("bookTitle") String bookTitle){
+        Optional<Book> bookBox = bookRepository.findByTitle(bookTitle);
+        if (bookBox.isEmpty()) {
+            return "redirect:/books";
+        }
+        model.addAttribute("book", bookBox.get());
+        return "bookDetails";
     }
 
     @GetMapping("/books/add")
